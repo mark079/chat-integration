7525f06beb5a82c758292423bb11adc5c8584256
Mark 079 <joaopedromaximo@gmail.com>
2023-11-30 16:26:46 -0300

    opa

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ app.post('/', async (req, res) => {
     const bracket1 = retorno.indexOf('[');
     const bracket2 = retorno.indexOf(']');
     const array = retorno.substring(bracket1, bracket2 + 1);
-    console.log('array', array);
     const json = JSON.parse(array);
     console.log('jsonJ', JSON.parse(array));
     res.json({ ...json });
